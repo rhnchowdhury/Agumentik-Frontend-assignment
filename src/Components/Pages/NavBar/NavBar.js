7997e40a6737815a2ea6729cae75daebfd9df83a
Raihan <rhnchdri@gmail.com>
2023-02-22 12:51:23 +0600

    navBar re

--- src/Components/Pages/NavBar/NavBar.js
+++ src/Components/Pages/NavBar/NavBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const menuItems = <React.Fragment>
-        <li><a>Find Reservations</a></li>
-        <li><a>Packages
+        <li><Link>Find Reservations</Link></li>
+        <li><Link>Packages
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-        </a></li>
-        <li><a>About Lakshadweep
+        </Link></li>
+        <li><Link>About Lakshadweep
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-        </a></li>
-        <li><a>About Us</a></li>
-        <li><a>Gol</a></li>
-        <li><a>Support</a></li>
+        </Link></li>
+        <li><Link>About Us</Link></li>
+        <li><Link>Gol</Link></li>
+        <li><Link>Support</Link></li>
     </React.Fragment>
     return (
         <div>
@@ -22,42 +23,19 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {/* <li><a>Item 1</a></li>
-                            <li tabIndex={0}>
-                                <a className="justify-between">
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li> */}
                             {menuItems}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        {/* <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a>
-                                Parent
-
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li> */}
                         {menuItems}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Get started</a>
+                    <Link className='mr-3' style={{ color: '#3282AD' }}>Login</Link>
+                    <Link className="badge p-5" style={{ backgroundColor: '#3282AD', border: '1px solid #3282AD' }}>Sign Up</Link>
                 </div>
             </div>
         </div>
